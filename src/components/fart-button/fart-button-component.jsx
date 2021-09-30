f322253960c7c797da7eadf428e33ab40d9bc242
Lewis Rosenberg-Smith <lrosenbergsmith@gmail.com>
2021-09-30 19:44:29 +1300

    made the fart button a poo

--- src/components/fart-button/fart-button-component.jsx
+++ src/components/fart-button/fart-button-component.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import useSound from 'use-sound';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPoop } from '@fortawesome/free-solid-svg-icons'
 
 import './fart-button-styles.scss'
 import Fart from '../../Audio/Fart.mp3'
 
+
+
 const FartButton = () => {
   const [play] = useSound(Fart);
+
   return <div>
-    <button 
-    class='button is-danger is-active is-centered'
-    onClick={play}>FART!
-    </button>
+    <FontAwesomeIcon icon={faPoop}
+    onClick={play}
+    />
     </div>
 };
 
